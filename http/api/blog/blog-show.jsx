e78b91d2b48fc15e94c565d13ff36b102f574f64
aida mousavi <aidamv12141722@gmail.com>
2025-12-08 11:03:14 +0330

    add try catch to api

--- http/api/blog/blog-show.jsx
+++ http/api/blog/blog-show.jsx
@@ -1,8 +1,27 @@
+import config from "@/config/appConfig";
+
 export async function blogShow(id) {
-  console.log(`https://ricksanchezz.ir/v1/blog/${id}`)
-    const res = await fetch(`https://ricksanchezz.ir/v1/blog/${id}`, {
-      cache: "force-cache", 
+  try {
+    const res = await fetch(`${config.apiBaseUrl}/blog/${id}`, {
+      cache: "force-cache",
+      next: { revalidate: 60 }, 
     });
-    return res.json();
+
+    if (!res.ok) {
+      throw new Error("خطا در دریافت اطلاعات بلاگ");
+    }
+
+    const result = await res.json();
+    return result.data;
+
+  } catch (error) {
+    console.error("❌ blogShow Error:", error);
+
+    return {
+      error: true,
+      message:
+        "در حال حاضر امکان دریافت اطلاعات وجود ندارد. لطفاً کمی بعد دوباره تلاش کنید.",
+      data: null,
+    };
   }
-  
+}
--- http/api/product/productShow.jsx
+++ http/api/product/productShow.jsx
@@ -1,12 +1,24 @@
-import config from "@/config/appConfig"
-export async function productShow(id=1) {
+import config from "@/config/appConfig";
 
-
-
+export async function productShow(id = 1) {
+  try {
     const res = await fetch(`${config.apiBaseUrl}/product/${id}`, {
-      next: { revalidate: 60 }
-    });
-    const result=await res.json()
-  
+      next: { revalidate: 60 },    });
+
+    if (!res.ok) {
+      throw new Error("خطا در دریافت محصول");
+    }
+
+    const result = await res.json();
     return result.data;
+
+  } catch (error) {
+    console.error("❌ productShow Error:", error);
+
+    return {
+      error: true,
+      message: "در حال حاضر امکان دریافت اطلاعات وجود ندارد. لطفاً کمی بعد دوباره تلاش کنید.",
+      data: null,
+    };
   }
+}
