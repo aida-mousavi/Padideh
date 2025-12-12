ae9ffd93d6bf14320b7192d98bec2df4c580d40f
reza.hosseinzadeh <rezarayan78@gmail.com>
2025-12-12 22:35:34 +0330

    Add redirect of محصولات to product-list

--- next.config.mjs
+++ next.config.mjs
@@ -10,12 +10,37 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "ricksanchezz.ir",
-        // pathname: "/other/project/**",
-      }
+      },
     ],
+  },
+
+  async redirects() {
+    return [
+      {
+        source: "/محصولات",
+        destination: "/product-list",
+        permanent: false,
+      },
+      // With / at the end
+      {
+        source: "/محصولات/",
+        destination: "/product-list",
+        permanent: false,
+      },
+      //  /محصولات/anything -> /product-list/anything
+      {
+        source: "/محصولات/:path*",
+        destination: "/product-list/:path*",
+        permanent: false,
+      },
+      //  Encoded URL
+      {
+        source: "/%D9%85%D8%AD%D8%B5%D9%88%D9%84%D8%A7%D8%AA",
+        destination: "/product-list",
+        permanent: false,
+      },
+    ];
   },
 };
 
 export default nextConfig;
-
-
